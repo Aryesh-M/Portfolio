219b1941803c24d05a07fb9afad5c0a5a5fec1cb
Yash <yashmarakna007@gmail.com>
2022-06-30 22:55:54 -0400

    The responsive website done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
+const hamburgerButton = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
 
 function toggleButton() {
-    navList.classList.toggle('show')
+  navList.classList.toggle("show");
 }
 
-hamburgerButton.addEventListener('click', toggleButton)
+hamburgerButton.addEventListener("click", toggleButton);
+
+function toggleMenu() {
+  let logo = document.getElementById("logo");
+  let aboutBtn = document.getElementById("about-btn");
+  let menuImage = document.getElementById("menu");
+  let imagePath = menuImage.src;
+  if (logo.style.display != "none") {
+    logo.style.display = "none";
+    aboutBtn.style.display = "none";
+    menuImage.src = imagePath.replace("menu", "error");
+  } else {
+    logo.style.display = "block";
+    aboutBtn.style.display = "block";
+    menuImage.src = imagePath.replace("error", "menu");
+  }
+}
